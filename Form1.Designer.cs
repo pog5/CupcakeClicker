df2e8debd8949f3f999aa87e412dcf6fa89b5e70
Nightly <pixelnightly@proton.me>
2023-07-15 23:57:05 +0300

    fuuck man

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,6 +41,16 @@
             label8 = new Label();
             button4 = new Button();
             button5 = new Button();
+            label3 = new Label();
+            label5 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button6 = new Button();
+            button7 = new Button();
+            label11 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -53,6 +63,7 @@
             label1.Size = new Size(361, 45);
             label1.TabIndex = 0;
             label1.Text = "Cupcake Clicker v440.13";
+            label1.Click += label1_Click;
             // 
             // label2
             // 
@@ -163,6 +174,7 @@
             // 
             // button4
             // 
+            button4.Enabled = false;
             button4.Location = new Point(302, 188);
             button4.Name = "button4";
             button4.Size = new Size(124, 23);
@@ -182,12 +194,124 @@
             button5.Visible = false;
             button5.Click += button5_Click;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Salmon;
+            label3.Location = new Point(501, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(285, 45);
+            label3.TabIndex = 13;
+            label3.Text = "DEVELOPER MENU";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.DarkSalmon;
+            label5.Location = new Point(542, 157);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 30);
+            label5.TabIndex = 16;
+            label5.Text = "Level:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.ForeColor = Color.DarkSalmon;
+            label9.Location = new Point(501, 127);
+            label9.Name = "label9";
+            label9.Size = new Size(106, 30);
+            label9.TabIndex = 15;
+            label9.Text = "Multiplier:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.ForeColor = Color.DarkSalmon;
+            label10.Location = new Point(501, 97);
+            label10.Name = "label10";
+            label10.Size = new Size(106, 30);
+            label10.TabIndex = 14;
+            label10.Text = "Cupcakes:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(613, 104);
+            textBox1.MaxLength = 19;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(173, 23);
+            textBox1.TabIndex = 17;
+            textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(613, 133);
+            textBox2.MaxLength = 19;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(173, 23);
+            textBox2.TabIndex = 18;
+            textBox2.Text = "1";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(613, 162);
+            textBox3.MaxLength = 19;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(173, 23);
+            textBox3.TabIndex = 19;
+            textBox3.Text = "1";
+            // 
+            // button6
+            // 
+            button6.Location = new Point(719, 188);
+            button6.Name = "button6";
+            button6.Size = new Size(67, 23);
+            button6.TabIndex = 20;
+            button6.Text = "SET";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(613, 188);
+            button7.Name = "button7";
+            button7.Size = new Size(100, 23);
+            button7.TabIndex = 21;
+            button7.Text = "GET CURRENT";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label11.ForeColor = Color.Tomato;
+            label11.Location = new Point(463, 214);
+            label11.Name = "label11";
+            label11.Size = new Size(387, 60);
+            label11.TabIndex = 22;
+            label11.Text = "Tip: You can click on the version number\r\n to unlock the Developer Menu early.\r\n";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlDarkDark;
             ClientSize = new Size(852, 277);
+            Controls.Add(label11);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label3);
             Controls.Add(button5);
             Controls.Add(button4);
             Controls.Add(button1);
@@ -223,5 +347,15 @@
         private Label label8;
         private Button button4;
         private Button button5;
+        private Label label3;
+        private Label label5;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button6;
+        private Button button7;
+        private Label label11;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,10 @@
-using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Drawing.Text;
-using System.Management;
 using System.Media;
-using System.Security.Principal;
 
 
 namespace ForceAdminClicker
 {
     public partial class Form1 : Form
     {
-
-        public static bool IsAdministrator()
-        {
-            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
-            {
-                WindowsPrincipal principal = new WindowsPrincipal(identity);
-                return principal.IsInRole(WindowsBuiltInRole.Administrator);
-            }
-        }
 
         public Form1()
         {
@@ -31,21 +16,18 @@ namespace ForceAdminClicker
                 Random rng = new Random();
                 Thread.Sleep(rng.Next(1200, 4500));
             }
-            else
-            {
-                cookies = 0;
-                cupcakecount.Text = cookies.ToString();
-                multiplier = 1;
-                cupcakecount.Text = multiplier.ToString();
-                level = 1;
-                label8.Text = level.ToString();
-            }
+            cookies = 0;
+            cupcakecount.Text = cookies.ToString();
+            multiplier = 1;
+            cupcakecount.Text = multiplier.ToString();
+            level = 1;
+            label8.Text = level.ToString();
         }
 
-        public int cookies { get; private set; }
-        public int multiplier { get; private set; }
-        public int level { get; private set; }
-        public int multiplierprice { get; private set; }
+        public Int64 cookies { get; private set; }
+        public Int64 multiplier { get; private set; }
+        public Int64 level { get; private set; }
+        public Int64 multiplierprice { get; private set; }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -55,9 +37,9 @@ namespace ForceAdminClicker
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (int.Parse(cupcakecount.Text) >= 20) { multiplierprice = int.Parse(cupcakecount.Text) * 2 * 100; }
-            else { multiplierprice = int.Parse(cupcakecount.Text) * 100; }
-            if (int.Parse(cupcakecount.Text) >= multiplierprice)
+            if (Int64.Parse(cupcakecount.Text) >= 20) { multiplierprice = Int64.Parse(cupcakecount.Text) * 2 * 100; }
+            else { multiplierprice = Int64.Parse(cupcakecount.Text) * 100; }
+            if (Int64.Parse(cupcakecount.Text) >= multiplierprice)
             {
                 cookies = cookies - multiplierprice;
                 cupcakecount.Text = cookies.ToString();
@@ -68,8 +50,9 @@ namespace ForceAdminClicker
             }
             else
             {
-                int remaining = multiplierprice - int.Parse(cupcakecount.Text);
-                MessageBox.Show($"You need {remaining} more cupcakes!");
+                Int64 remaining = multiplierprice - Int64.Parse(cupcakecount.Text);
+                string text = $"You need {remaining} more cupcakes!";
+                MessageBox.Show(text, "Unmet requirements", MessageBoxButtons.OK, MessageBoxIcon.Stop);
             }
         }
 
@@ -77,15 +60,28 @@ namespace ForceAdminClicker
         {
             if (multiplier >= 50)
             {
-
+                cookies = multiplier * 14 * level;
+                cupcakecount.Text = cookies.ToString();
+                multiplier = 1;
+                multipliercount.Text = multiplier.ToString();
+                level = level + 1;
+                label8.Text = level.ToString();
+                if (level >= 10)
+                {
+                    button4.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Multiplier 50 needed.", "Unmet requirements", MessageBoxButtons.OK, MessageBoxIcon.Stop);
             }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (int.Parse(label8.Text) >= 1)
+            if (level >= 10)
             {
-                MessageBox.Show("GG!");
+                MessageBox.Show("GG!", "You win! :D", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 button5.Show();
             }
         }
@@ -97,12 +93,43 @@ namespace ForceAdminClicker
                 button5.Text = "Hide <<<";
                 this.ClientSize = new Size(852, 277);
 
-            } else
+            }
+            else
             {
                 button5.Text = "Show >>>";
                 this.ClientSize = new Size(452, 277);
             }
         }
 
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = cookies.ToString();
+            textBox2.Text = multiplier.ToString();
+            textBox3.Text = level.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cookies = Int64.Parse(textBox1.Text);
+                multiplier = Int64.Parse(textBox2.Text);
+                level = Int64.Parse(textBox3.Text);
+                multiplierprice = Int64.Parse(cupcakecount.Text) * 100;
+                cupcakecount.Text = cookies.ToString();
+                multipliercount.Text = multiplier.ToString();
+                label8.Text = level.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("You either: \n 1. Put something bigger than 9,223,372,036,854,775,807\n 1. Put something smaller than -9,223,372,036,854,775,807\n 2. Did not put a actual number\n 3. Forgot to fill in some fields", "Error while setting values", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            level = 10;
+            button4.Enabled = true;
+        }
     }
 }
